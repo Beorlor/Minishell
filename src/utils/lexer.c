9cbc4f2aa720c6eb6911333d22ede7cb4335f151
Beorlor <jbdurand2000@gmail.com>
2024-02-28 15:11:43 +0100

    enum lexer

--- src/utils/lexer.c
+++ src/utils/lexer.c
@@ -5,25 +5,27 @@
 // BEHAVIOR : input a string and return a link list of t_token
 
 typedef enum {
-    TOKEN_LEFT_PAREN, // (
-    TOKEN_RIGHT_PAREN, // )
+    TOKEN_PAREN, // (
     TOKEN_COMMAND, // Command name
-    TOKEN_ARGUMENT, // Argument
+    // TOKEN_ARGUMENT, // Argument
     TOKEN_LOGICAL_AND, // &&
     TOKEN_LOGICAL_OR, // ||
     TOKEN_PIPE, // |
     TOKEN_REDIRECTION_IN, // <
     TOKEN_REDIRECTION_OUT, // >
     TOKEN_REDIRECTION_APPEND, // >>
-    TOKEN_REDIRECTION_ERROR, // 2>
-    TOKEN_HERE_DOCUMENT, // <<
-	TOKEN_LIMITER, // Delimiter for here-document
+
+	// A VOIR
+    //TOKEN_REDIRECTION_ERROR, // 2>
+
+	// TO MANAGE BEFORE PARSER !!!
+    //TOKEN_HERE_DOCUMENT, // <<
+	// TOKEN_LIMITER, // Delimiter for here-document
+
     TOKEN_WILDCARD, // *
-    TOKEN_ENV_VAR, // $VAR
-    TOKEN_EXIT_STATUS, // $?
-	TOKEN_STRING, // Generic string
-    TOKEN_SINGLE_QUOTE_STRING, // '
-    TOKEN_DOUBLE_QUOTE_STRING // "
+
+    // TOKEN_ENV_VAR, // $VAR
+    // TOKEN_EXIT_STATUS, // $?
 } TokenType;
 
 //gere ton le ; pour avoir plusieurs commande sur une meme ligne ???? Je pense que ce serait mieux oui mais toujours du taff en + (thomas)
